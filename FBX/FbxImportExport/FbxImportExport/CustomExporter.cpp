f8f93c121499e7008734d42921d231b6ea11c8f4
narkos <narkos.newb@gmail.com>
2019-03-28 19:46:15 +0100

    super basic readwrite

--- FBX/FbxImportExport/FbxImportExport/CustomExporter.cpp
+++ FBX/FbxImportExport/FbxImportExport/CustomExporter.cpp
@@ -18,3 +18,48 @@
 //{
 //
 //}
+
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include "CustomExporter.h"
+
+using namespace std;
+
+
+namespace BinaryWriter {
+	
+	void CreateFile() {
+		ofstream myfile;
+		myfile.open("example.txt");
+		myfile << "Writing some text. \n";
+		myfile << "Writing some more text \n";
+		myfile.close();
+		cout << "ok this works " << endl;
+		//BinaryWriter::Vertex vert ( 'test', [1, 2, 3] );
+
+		Vertex vert = { "TestName", { 1, 2, 3} };
+		/*strcpy(vert.name, "Testname");
+		vert.position = { 1, 2, 3 }*/;
+		//vert.name = "test";
+		cout << sizeof(vert.name) << endl;
+		cout << sizeof(vert.position) << endl;
+		cout << sizeof(vert) << endl;
+
+		ofstream myBinFile;
+		myBinFile.open("example.lul", ios::out | ios::binary);
+		myBinFile.write((char*)vert.name, sizeof(vert.name));
+		myBinFile.write((char*)vert.position, sizeof(int) * 3);
+		myBinFile.close();
+
+		char test[100];
+		ifstream myReadFile;
+		myReadFile.open("example.lul", ios::binary | ios::in);
+		myReadFile.read(test, sizeof(vert.name));
+		cout << test << endl;
+		myReadFile.read(test, sizeof(int)* 3);
+		cout << (int)test[0] << (int)test[4] << (int)test[8] << endl;
+		myReadFile.close();
+		return;
+	}
+}
--- FBX/FbxImportExport/FbxImportExport/CustomExporter.h
+++ FBX/FbxImportExport/FbxImportExport/CustomExporter.h
@@ -14,3 +14,16 @@
 //		void WriteToFile(string fileName, File *data);
 //	};
 //}
+namespace BinaryWriter {
+	void CreateFile();
+	struct Vertex 
+	{
+		char name[50];
+		int position[3];
+		/*Vertex(char* p_name, int p_pos[3])
+		{
+			name = p_name;
+			position = p_pos;
+		}*/
+	};
+}
